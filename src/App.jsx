1417bf9d4bb7a3f97660cd7c1e6c9c749364f0a1
RAJHIMANSHUPATEL <blazehimanshuraj@gmail.com>
2024-09-21 02:36:56 +0530

    index.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import { useState } from 'react'
-import {InputBox} from "./components"
+import {InputBox} from "./components/index.js"
 import useCurrencyInfo from './hooks/useCurrencyInfo'
 
 function App() {
